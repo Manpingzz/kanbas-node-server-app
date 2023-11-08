b7b24f6b85587cebfb2a91cd4d0b1e4626204a45
Manpingzz <zhao.manp@northeastern.edu>
2023-11-08 00:26:06 -0800

    Implementing Routes for Courses

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import "dotenv/config";
-import CourseRoutes from "./courses/routes.js"
-import ModuleRoutes from "./modules/routes.js"
+import CourseRoutes from "./courses/routes.js";
+// import ModuleRoutes from "./modules/routes.js"
 
 // const express = require('express')
 const app = express();
-app.use(
-	cors({
-		credentials: true,
-		origin: process.env.FRONTEND_URL,
-	})
-)
+app.use(cors());
 // Passing JSON data to the server in an HTTP Body
 app.use(express.json())
-const PORT = 4000
+CourseRoutes(app);
 
 Hello(app)
-Lab5(app)
-CourseRoutes(app)
-ModuleRoutes(app)
 
-app.listen(process.env.PORT || PORT)
+Lab5(app)
+// ModuleRoutes(app)
+
+app.listen(4000);
