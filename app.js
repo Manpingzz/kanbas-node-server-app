be67b67da102fd0fed94f773758b3e5273f48993
Manpingzz <zhao.manp@northeastern.edu>
2023-11-12 14:27:10 -0800

    update

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import "dotenv/config";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js"
+import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
- 
 
 // const express = require('express')
 const app = express();
-app.use(cors());
-app.use(express.json())
-const PORT = 4000
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
-
+// app.use(cors());
+app.use(express.json());
+// const PORT = 4000;
 
 Hello(app);
 Lab5(app);
@@ -23,5 +27,3 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 
 app.listen(process.env.PORT || 4000);
-
-
