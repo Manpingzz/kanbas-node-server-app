7e58fe1f8fb3194f09df406f10310dbe436b51d9
Manpingzz <zhao.manp@northeastern.edu>
2023-11-27 14:03:40 -0800

    Update

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
@@ -25,6 +26,14 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(session(sessionOptions));
 
 app.use(express.json());
