daeb71c02d402f092413907c5fa24ef6b2ad32b8
Manpingzz <zhao.manp@northeastern.edu>
2023-11-27 00:16:39 -0800

    finished 3.5.10

--- app.js
+++ app.js
@@ -3,31 +3,42 @@ import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 
 // const express = require('express')
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:3000",
-      "https://thunderous-daffodil-22ffea.netlify.app",
-    ],
+    origin: "http://localhost:3000",
   })
 );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+
+app.use(express.json());
 
 // app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 // const PORT = 4000;
+UserRoutes(app);
 
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
