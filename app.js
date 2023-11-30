1ea20c4172f4e879f3399e3e317041b33b657456
Manpingzz <zhao.manp@northeastern.edu>
2023-11-29 16:12:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ import session from "express-session";
 
 // const express = require("express");
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 // mongoose.connect(CONNECTION_STRING);
 console.log(CONNECTION_STRING);
 mongoose
